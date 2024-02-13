828539bb55273e29bbe2be619b0da25fd2f3a535
SubhutiKapse <subhutikapse799@gmail.com>
2024-02-14 00:35:26 +0530

    loops.js

---
+++ loops.js
@@ -0,0 +1,39 @@
+//for(initialization;CSSConditionRule;increment){
+    //statements
+//}
+
+//program 1
+for (let i=1; i<=3;i++){
+    console.log(i)
+}
+
+//program 2
+for (let i=1; i<=5;i++) {
+    console.log(i)}
+
+//program 3
+//print 10 to 1
+for (let i=10;i>1;i--) {
+    console.log(i)
+}   
+
+//program 4
+for (let i=1 ;i<=10;i++){
+    console.log(i)
+}
+//table of 2
+for (let i=2 ;i<=20;i=i+2){
+    console.log(i)
+}
+
+
+//program 5
+//print table in reverse order
+
+for(let i=50;i>=5;i=i-5){
+    console.log(i)
+}
+
+for (let i=70;i>=7;i=i-7){
+    console.log(i)
+}
