f2e93c5ad883a9780938800c40404919db2310a1
SubhutiKapse <subhutikapse799@gmail.com>
2024-02-22 00:14:01 +0530

    arraymethod2

--- arraymethod2.js
+++ arraymethod2.js
@@ -101,3 +101,149 @@ for (let i=0;i<city.length;i++){
     //console.log(city[i])
     console.log("welcome "+city[i])
 }
+
+
+
+
+//program 1
+let birthYear=[1989,1990,1991,1992]
+let ages=[]
+for (let i=0;i<birthYear.length;i++){
+    //console.log(i)
+    //console.log(birthYear[i])
+    let x=2024-birthYear[i]
+    ages.push(x)
+    
+}
+
+console.log(ages)
+
+//map
+birthyr=[1989,1990,1991,1992,1993]
+let q1=birthyr.map(function(el,index,arr){
+     return 2024-el
+})
+console.log(q1)
+
+let numT=[11,22,33,44,55,66,77]//+1
+let w2=numT.map(function(el,index,arr){
+    return el+1
+})
+console.log(w2)
+
+//program 2
+let Marks=[55,66,77,88,99,100]
+let above50=[]
+for (let i=0;i<marks.length;i++){
+    if (Marks[i]>50){
+        above50.push(Marks[i])
+
+    }
+}
+console.log(above50)
+
+//filter
+let e4= marks.filter(function(e4,index,arr){
+    return e4 > 50
+})
+console.log(e4)
+
+//program
+let transactions=[11,22,33,44,55,-66,-77]
+let deposit =transactions.filter(function(e8,index,arr){
+    return e8 > 0
+})
+console.log(deposit)
+let withdrawl =transactions.filter(function(e8,index,arr){
+    return e8 < 0
+})
+console.log(withdrawl)
+
+//program 3 reduce
+let NUM=[11,22,33] //sum of arraays
+let total7 =0
+for (let i=0;i<NUM.length;i++){
+    total7=total7+NUM[i]
+}
+console.log(total1)
+let e7=NUM.reduce(function(acc,e1,index,arr){
+    return acc+e1
+})
+console.log(e7)
+
+//map
+let Birthyear=[1980,1981,1982,1983]
+let q11=Birthyear.map(function(el,index,arr){
+    return 2024-el
+
+})
+console.log(q11)
+
+let NUMs=[11,22,33,44,55]//add 10
+let d1=NUMs.map(function(el,index,arr){
+    return el+10
+
+})
+console.log(d1)
+
+//filter() 
+let markS=[23,55,47,89,76,99]
+k9=markS.filter(function(el,index,arr){
+return el>20 && el<88
+})
+console.log(k9)
+
+//reduce() 
+let roll=[12,34,45,67,88]//addition
+o0=roll.reduce(function(acc,el,index,arr){
+    return acc+el
+
+
+})
+console.log(o0)
+
+//forEach()
+let cities=["pune","mumbai","banglore","hyderabad"]
+p0=cities.forEach(function(el,index,arr){
+console.log("Hello "+el)
+})
+console.log(p0)
+
+//filter()
+let ele=[11,22,33,44,55]//above 30
+p1=ele.filter(function(el,index,acc){
+    return el>30
+})
+console.log(p1)
+//find()
+let a1=ele.find(function(el,index,arr){
+return el<40
+})
+//findIndex()
+//let no=[34,45,67,78,89]
+
+// let l1=no.findIndex(function(el,index,arr){
+//     return el>20
+// })
+// console.log(l1)
+//every() returns boolean values true/false
+let numh=[11,2,34,33,45,55,65,65,78]
+let y1=numh.every(function(el,index,arr){
+return el>10
+})
+console.log(y1)
+//some()
+let d3= numh.some(function(el,index,arr){
+    return el >540
+
+})
+console.log(d3)
+//slice()
+
+let numm=[111,222,333,444,555,666,777,888,999]
+//numm.slice(startinded,endindex(not included))
+console.log(numm.slice(3))
+console.log(numm.slice(-3))
+console.log(numm.slice(-10,4))
+console.log(numm.slice(1,-6))
+console.log(numm.slice(-1,-3))
---
+++ revision2.js
@@ -0,0 +1,62 @@
+let num=178
+if (num >0 && num>12){
+    console.log("10 % discount")
+}
+if (num <87 && num>182){
+    console.log("20 % discount")
+}
+if (num>34 && num>312){
+    console.log("30 % discount")
+}
+
+//program 2
+//if else
+numT=17
+if (num >0 && num>12){
+    console.log("10 % discount")
+}
+else if (num <87 && num>182){
+    console.log("20 % discount")
+}
+else if (num>34 && num>312){
+    console.log("30 % discount")
+}
+else {
+    console.log("imcorrect input")
+}
+
+//program 3
+marks=91
+if(marks >90){
+    console.log("grade A")
+}
+if(marks >90){
+    console.log("grade B")
+}
+if(marks >90){
+    console.log("grade C")
+}
+
+
+//program 4
+marks=91
+if(marks >90){
+    console.log("grade A")
+}
+else if(marks >90){
+    console.log("grade B")
+}
+else if(marks >90){
+    console.log("grade C")
+}
+else ("fail")
+
+//program 5
+let a=100
+let b=500
+if (a>b){
+    console.log("a is greater")
+}
+if (b>a){
+    console.log("b is greater")
+}
